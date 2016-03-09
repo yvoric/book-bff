2e5598643580ef278d55365d34cfa2087c304e27
ityvrich <yvonne.richa@schibsted.no>
2016-03-09 12:35:18 +0100

    pickRelevantBookData fix

--- routes/book.js
+++ routes/book.js
@@ -6,22 +6,19 @@ var jp = require('jsonpath');
 var router = new express.Router();
 var ESI = require('nodesi');
 var esi = new ESI({
-        onError: (src, error) => `<!-- GET ${src} resulted in ${error} -->`
+    onError: (src, error) => `<!-- GET ${src} resulted in ${error} -->`
 });
 
 var BOOK_SERVICE_URL = 'https://book-catalog-proxy-5.herokuapp.com/book?isbn=';
-//var BOOK_COUNT_URL = 'http://book-service-gregers.herokuapp.com/stock';
-var BOOK_COUNT_URL = process.env.BOOK_COUNT_URL || 'https://yvonnes-book-inventory-service.herokuapp.com/stock';
+var BOOK_COUNT_URL = process.env.BOOK_COUNT_URL || 'http://book-service-gregers.herokuapp.com/stock';
 
-function pickRelevantBookData (isbn, data) {
+function pickRelevantBookData (data) {
     const volume = jp.value(data, '$..volumeInfo');
-
 
     return {
         bookTitle: volume.title,
         subtitle: volume.subtitle,
-        bookCover: jp.value(volume, '$..thumbnail'),
-        stockCountURL: `${BOOK_COUNT_URL}/${isbn}`
+        bookCover: jp.value(volume, '$..thumbnail')
     };
 }
 
@@ -40,9 +37,9 @@ function partial (fn, args) {
 
 router.get('/:isbn', function (req, res, next) {
     const isbn = req.params.isbn;
+    const requestId = req.headers['x-request-id'];
     goodGuy(`${BOOK_SERVICE_URL}${isbn}`)
         .then(response => JSON.parse(response.body))
-        .then(partial(pickRelevantBookData, isbn))
         .then(pickRelevantBookData)
         .then(bookData => Object.assign({ stockCountURL: `${BOOK_COUNT_URL}/${isbn}`, requestId }, bookData))
         .then(partial(renderPage, req.app))
